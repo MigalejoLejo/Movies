f9890b96e655e67c0101aba2f5f67a5b9915f066
Miguel Alejandro Correa (PWC) <miguelalejandro.correa.avila@pwc.com>
2023-06-05 08:12:24 +0100

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Miguel Alejandro Correa Avila on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
